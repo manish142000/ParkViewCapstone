e733855c4beaf7c5d233e3319d4e0c640e2d9c1a
manish142000 <bawkarmanish14@email.com>
2023-09-04 23:14:26 +0530

    Added Google Authorisation

--- ParkView/Program.cs
+++ ParkView/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ParkView.Models;
 using Microsoft.AspNetCore.Identity;
 using System.Composition;
+using System.Text.Json.Serialization;
 
 namespace ParkView
 {
@@ -15,13 +16,25 @@ namespace ParkView
             builder.Services.AddScoped<IRoomCategory, RoomCategoryDbRepo>();
             builder.Services.AddScoped<IHotel, HotelDbRepo>();
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 
+            builder.Services.AddRazorPages();
 
             builder.Services.AddDbContext<HotelDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
                         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<HotelDbContext>();
+
+            builder.Services.AddAuthentication().AddGoogle(
+                googleoptions =>
+                {
+                    googleoptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+                    googleoptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+                });
 
             var app = builder.Build();
 
@@ -46,6 +59,8 @@ namespace ParkView
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
 
             app.Run();
         }
